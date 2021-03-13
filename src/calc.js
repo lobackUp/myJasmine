ace53f413b836bfb89091e6e224248d97eec8708
lotiko <l.despres@netc.fr>
2021-03-13 21:20:49 +0100

    I find the issue lol !o)

--- src/calc.js
+++ src/calc.js
@@ -10,3 +10,15 @@ function sum(array) {
 
   return array.reduce((acc, value) => acc + value, 0);
 }
+
+function avg(array) {
+  if (array === undefined) {
+    throw new Error("No parameter provided");
+  }
+
+  // if (!Array.isArray(arr)) return 0;
+  if (array.length === 0) return 0;
+
+  let sumN = sum(array);
+  return sumN / array.length;
+}
