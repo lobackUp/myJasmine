c1755dd35c2041bc66f263c598031f2fbba7714d
lotiko <l.despres@netc.fr>
2021-03-13 21:08:33 +0100

    what's the goal?

--- src/calc.js
+++ src/calc.js
@@ -1,3 +1,12 @@
 // src/calc.js
 
-function sum(array) {}
+function sum(array) {
+  if (array === undefined) {
+    throw new Error("No parameter provided");
+  }
+
+  // if (!Array.isArray(arr)) return 0;
+  if (array.length === 0) return 0;
+
+  return array.reduce((acc, value) => acc + value, 0);
+}
